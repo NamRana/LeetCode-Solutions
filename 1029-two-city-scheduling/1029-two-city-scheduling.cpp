136956be6bb47f214bda508b80ce4a176f99cf48
Naman Rana <91771208+NamRana@users.noreply.github.com>
2023-04-21 14:26:09 +0530

    Time: 8 ms (38.87%), Space: 7.9 MB (52.91%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int twoCitySchedCost(vector<vector<int>>& nums) {
+        
+        int ans=0;
+        vector<int> d;
+        
+        for(int i=0;i<nums.size();i++){
+            ans+=nums[i][0];
+            d.push_back(nums[i][1]-nums[i][0]);
+        }
+        sort(d.begin(),d.end());
+        
+        for(int i=0;i<nums.size()/2;i++){
+            ans+=d[i];
+        }
+        return ans;
+    }
+};
